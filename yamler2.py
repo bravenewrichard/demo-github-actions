5dca74c35faf93296f922fa3e7cc4787d930e9ac
Richard Pappalardo <rpappalax@gmail.com>
2020-12-21 11:03:41 -0800

    fix: write error

--- yamler2.py
+++ yamler2.py
@@ -2,11 +2,19 @@ fname = "bitrise.yml"
 
 new_stack = 'osx-xcode-12.1.x'
 
+"""
 with open(fname, 'r') as f:
     s = f.read() 
 
 with open(fname, 'w') as f:
     s = s.replace('<stack>', new_stack)
     f.write(s)
+"""
 
 
+
+bitriseyml = root / "bitrise.yml"
+index_txt = "\n".join(index).strip()
+bitriseyml_contents = bitriseyml.open().read()
+bitriseyml_contents = bitriseyml_contents.replace('<stack>', new_stack)
+bitriseyml.open("w").write(bitriseyml_contents)
